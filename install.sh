5e7a4467ce002dfcca6fd433bf2019c19c97023b
PreslavMihaylov <preslav@pmihaylov.com>
2018-01-07 14:58:57 +0200

    added scripts to installation

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# make sure directory path is ok when script is started from anywhere
 MYDIR="$(dirname -- "$0")"
 
+# .vimrc install
 cp $MYDIR/vimrc ~/.vimrc
-rm -rf ~/.vim
+
+# .vim install
+rm -rf ~/.vim || true
 cp -r $MYDIR/vim ~/.vim
+
+# .bash_aliases install
 cp $MYDIR/bash_aliases ~/.bash_aliases
+
+# scripts install
+rm -rf ~/scripts || true
+cp -r $MYDIR/scripts ~/scripts
